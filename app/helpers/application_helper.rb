d385118cc306bd4a7fec627f73e4bc62f7c2d81f
rodrigopk <rodrigopk@gmail.com>
2019-04-14 20:03:48 +0200

    F [#AXETNymK]: Add footer with external links
    Added links to github repo and dungeonmastertools

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  PROJECT_GITHUB_URL = 'https://github.com/rodrigopk/hiperbolic_treasure_chamber'
+  DUNGEONMASTERTOOLS_URL = 'https://dungeonmastertools.github.io'
+
+  def github_url
+    PROJECT_GITHUB_URL
+  end
+
+  def dm_toos_url
+    DUNGEONMASTERTOOLS_URL
+  end
 end
